085f30171147234a9559d88d816438058e1b0e0c
kapil jhajhria <jhajhria44@gmail.com>
2019-10-07 10:16:09 +0530

    Completed

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Point {
   int x;
   int y;
@@ -14,7 +16,6 @@ class Point {
 // Bottom-left: (-1, 0)
 // Top-right: (2,3)
 
-
 // Challenge 2
 // A number is a perfect number if is equal to sum of its proper divisors, that is, sum of its positive divisors excluding the number itself.
 // Write a function to check if a given number is perfect or not.
@@ -27,6 +28,36 @@ void main() {
   // Test the function defined above with given List of points
   List<Point> points = [Point(-1, 0), Point(2, 2), Point(1, 3)];
 
+  rectangle(points);
   // Test the perfect number function here
+  print(perfectNumber(6));
+}
 
+bool perfectNumber(int x) {
+  int sum = 0;
+  for (int i = 1; i <= x ~/ 2; i++) {
+    if (x % i == 0) {
+      sum = sum + i;
+    }
+  }
+  print(
+      "Debug Statement: value of sum is $sum"); // for debug, to check value of sum
+  if (sum == x) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void rectangle(List<Point> pt) {
+  List<int> x = pt.map((Point pointOject) => pointOject.x).toList();
+  List<int> y = pt.map((Point pointOject) => pointOject.y).toList();
+
+  int xmin = x.reduce(min);
+  int xmax = x.reduce(max);
+
+  int ymin = y.reduce(min);
+  int ymax = y.reduce(max);
+  print('Bottom-left: ($xmin, $ymin)');
+  print('Top-right: ($xmax,$ymax)');
 }
